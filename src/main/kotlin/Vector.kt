a344da94fb7851015e5ba1674c3c1b271331b332
Mao <doryantilmant25@gmail.com>
2022-12-11 18:15:48 +0100

    finished Vector class

--- src/main/kotlin/Vector.kt
+++ src/main/kotlin/Vector.kt
@@ -1,7 +1,18 @@
+import kotlin.math.sqrt
+
 data class Vector(var x: Int, var y: Int) {
     fun add(vector: Vector) {
         x += vector.x
         y += vector.y
+    }
+
+    fun sub(vector: Vector) {
+        x -= vector.x
+        y -= vector.y
+    }
+
+    fun copy() : Vector {
+        return Vector(x, y)
     }
 
     fun mult(m: Double) {
@@ -14,7 +25,22 @@ data class Vector(var x: Int, var y: Int) {
         y *= vector.y
     }
 
-    fun mag() {
+    fun mag() : Double {
+        return sqrt((x * x + y * y).toDouble())
+    }
 
+    fun normalize() {
+        this.mult(1 / this.mag())
+    }
+
+    fun dist(vector: Vector) : Double {
+        val copy = this.copy()
+        copy.sub(vector)
+        return copy.mag()
+    }
+
+    fun setMag(mag: Double) {
+        this.normalize()
+        this.mult(mag)
     }
 }
